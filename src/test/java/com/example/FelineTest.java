6a9beba6e12e7483f177666a5ec8685d8cceb5f5
Anna Prozorovich <smoragd@gmail.com>
2025-01-28 21:03:52 +0700

    Sprint_6 Commit_2

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -3,39 +3,38 @@ package com.example;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
-
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Mock
+    // Изменила мок на шпиона, тк в классе Feline метод getKittens()
+    // вызывает другой метод getKittens(int kittensCount) того же класса.
+    // Переписала тест для getKittens().
+    @Spy
     Feline feline = new Feline();
 
     @Test
     public void eatMeatSuccess() throws Exception {
-        when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
     }
 
     @Test
     public void getFamilySuccess() throws Exception {
-        when(feline.getFamily()).thenReturn("Кошачьи");
         Assert.assertEquals("Кошачьи", feline.getFamily());
     }
 
     @Test
     public void getKittensNoParams() throws Exception {
-        when(feline.getKittens()).thenReturn(1);
-        Assert.assertEquals(1, feline.getKittens());
+        feline.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens(1);
     }
 
     @Test
     public void getKittensWithParams() throws Exception {
-        when(feline.getKittens(5)).thenReturn(5);
         Assert.assertEquals(5, feline.getKittens(5));
     }
 
